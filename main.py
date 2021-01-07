12b327ac65925d77d1439a23a728cfca7b4270e7
Ciesiu <ciesinski.kontakt@gmail.com>
2021-01-07 22:42:01 +0100

    Zad4

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import pandas as pd
 from sklearn import svm
 from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, f1_score, roc_auc_score
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+from sklearn.tree import DecisionTreeClassifier  # Import Decision Tree Classifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import time
 
 X_train = None
 X_test = None
@@ -25,7 +26,8 @@ def zad1():
     y_test = pd.read_csv('./Test/y_test.txt', sep=" ", header=None)
     print(y_test)
 
-def svm_(): #underscore to not override built in function
+
+def svm_():  # underscore to not override built in function
     global X_train, X_test, y_train, y_test
     X = X_train
     y = y_train
@@ -35,6 +37,7 @@ def svm_(): #underscore to not override built in function
     print('SVM: ')
     print(predicted)
     return predicted
+
 
 def knn():
     global X_train, X_test, y_train, y_test
@@ -50,6 +53,7 @@ def knn():
     print(predicted)
     return predicted
 
+
 def decisionTree():
     global X_train, X_test, y_train, y_test
     X = X_train
@@ -62,6 +66,7 @@ def decisionTree():
     print('Decision Tree: ')
     print(predicted)
     return predicted
+
 
 def randomForest():
     global X_train, X_test, y_train, y_test
@@ -76,12 +81,14 @@ def randomForest():
     print(predicted)
     return predicted
 
+
 def zad2():
-    global svmLabels,knnLabels,dtLabels,rfLabels
+    global svmLabels, knnLabels, dtLabels, rfLabels
     svmLabels = svm_()
     knnLabels = knn()
     dtLabels = decisionTree()
     rfLabels = randomForest()
+
 
 def calculateConfusionMatrix(y, y_test):
     cm = confusion_matrix(y, y_test).ravel()
@@ -93,25 +100,32 @@ def calculateConfusionMatrix(y, y_test):
     # print('Confusion matrix')
     # print((tn, fp, fn, tp))
 
+
 def calculateACC(y, y_test):
     acc = accuracy_score(y, y_test)
     print("ACC:")
     print(acc)
+    return acc  # return dodany na potrzeby zadania 4
+
 
 def calculateRecall(y, y_test):
     rec = recall_score(y, y_test, average='weighted')
     print("Recall:")
     print(rec)
 
+
 def calculateF1(y, y_test):
     f1 = f1_score(y, y_test, average='weighted')
     print("F1")
     print(f1)
+    return f1  # return dodany na potrzeby zadania 4
+
 
 def calculateAUC(y, y_test):
     auc = roc_auc_score(y, y_test, multi_class='ovo')
     print("AUC")
     print(auc)
+
 
 def zad3():
     # confusion matrix
@@ -138,11 +152,12 @@ def zad3():
     calculateF1(dtLabels, y_test)
     calculateF1(rfLabels, y_test)
 
-    # AUC nie wiem, niby wymaga normalizacji danych żeby działać
+    # AUC niby wymaga normalizacji danych żeby działać
     # calculateAUC(svmLabels, y_test)
     # calculateAUC(knnLabels, y_test)
     # calculateAUC(dtLabels, y_test)
     # calculateAUC(rfLabels, y_test)
+
 
 def svm_CV():
     global X_train, y_train
@@ -158,13 +173,13 @@ def svm_CV():
     clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
     clf.score(X_test, y_test)
 
-
     clf = svm.SVC()
     clf.fit(X, y.values.ravel())
     predicted = clf.predict(X_test)
     print('SVM CV: ')
     print(predicted)
     return predicted
+
 
 def knn_CV():
     global X_train, y_train
@@ -187,6 +202,7 @@ def knn_CV():
     print(predicted)
     return predicted
 
+
 def decisionTree_CV():
     global X_train, y_train
     X = X_train
@@ -206,6 +222,7 @@ def decisionTree_CV():
     print('Decision Tree: ')
     print(predicted)
     return predicted
+
 
 def randomForest_CV():
     global X_train, y_train
@@ -227,21 +244,81 @@ def randomForest_CV():
     print(predicted)
     return predicted
 
+
 def zad3_2():
-    #Cross Validation is a technique which involves reserving a particular sample of a dataset
-    #on which you do not train the model.
-    #Later, you test your model on this sample before finalizing it.
+    # Cross Validation is a technique which involves reserving a particular sample of a dataset
+    # on which you do not train the model.
+    # Later, you test your model on this sample before finalizing it.
     svm_CV()
     knn_CV()
     decisionTree_CV()
     randomForest_CV()
 
 
+def zad4():
+    global X_train, X_test, y_train, y_test
+
+    # Na podstawie wskaźników z zadania 3, dla domyślnych parametrów najlepiej sobie radził SVM, więc tego będę się trzymał
+    # gdybym miał testować kombinacje wszystkich parametrów to program wykonywałby się 3 dni
+    # dlatego będę manipulował 6 wybranymi parametrami, dla każdego z nich kilka wariantów
+    # co samo w sobie daje dużo przebiegów samego algorytmu uczącego + sprawdzenie jego wskaźników
+    C = [1, 3]
+    degree = [1, 5]
+    kernel = ['linear', 'poly', 'rbf', 'sigmoid']
+    gamma = ['auto', 'scale']
+    coef0 = [0, 2]
+    decision_function_shape = ['ovo', 'ovr']
+
+    bestA = bestB = bestC = bestD = bestE = bestF = None  # 'optymalne' parametry
+    bestACC = bestF1 = 0  # sprawdzam tylko ACC i F1 dla zaoszczędzenia czasu
+    loopCounter = 1  # licznik żebym wiedział co się dzieje
+    # przy powyższym zestawie parametrów uczenie wykona się 2*2*4*2*2*2 = 128 razy
+    startTime = time.time()
+    for a in C:
+        for b in degree:
+            for c in kernel:
+                for d in gamma:
+                    for e in coef0:
+                        for f in decision_function_shape:
+                            print("Wykonuję kombinację numer: " + str(loopCounter))
+                            clf = svm.SVC(C=a, degree=b, kernel=c, gamma=d, coef0=e, decision_function_shape=f)
+                            clf.fit(X_train, y_train.values.ravel())
+                            predicted = clf.predict(X_test)
+                            acc = calculateACC(predicted, y_test)
+                            f1 = calculateF1(predicted, y_test)
+                            # print('WSKAZNIKI')
+                            # print(acc)
+                            # print(f1)
+                            aver = (acc + f1) / 2
+                            oldAver = (bestACC + bestF1) / 2
+                            if (aver > oldAver):
+                                print("Jest nowy najlepszy")
+                                bestA = a
+                                bestB = b
+                                bestC = c
+                                bestD = d
+                                bestE = e
+                                bestF = f
+                                bestACC = acc
+                                bestF1 = f1
+                            else:
+                                if(aver == oldAver):
+                                    print("To samo co najlepszy")
+                                else:
+                                    print("Wcześniej było lepiej")
+                            loopCounter = loopCounter + 1
+    stopTime = time.time()
+    print("Zakończyłem sprawdzanie: "+str(bestA)+"; "+str(bestB)+"; "+bestC+"; "+bestD+"; "+str(bestE)+"; "+bestF)
+    print("Wykonywalem się: "+str(((stopTime-startTime) / 60))+" minut")
+    return [bestA, bestB, bestC, bestD, bestE, bestF]
+
+
 zad1()
 zad2()
 zad3()
 zad3_2()
+###### Zad4 - nie uruchamiać bez potrzeby
+###### przy bieżących ustawieniach na mojej maszynie funkcja wykonywała się 53 minuty
+###### i zwróciła wynik [1, 5, poly, scale, 2, ovo]
+# zad4()
 
-#IMPORTANT NOTES:
-# - IN ZAD1 READ MAYBE SEPARATELY X AND y
-# - TO 'FIT' FUNCTIONS USE TRAIN DATA, TO PREDICT - TEST DATA
